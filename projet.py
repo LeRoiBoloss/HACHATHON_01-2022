e5ebc83c5dee022ba4fe78e4e94c0802521fef27
HAMZAOUI Amin <amin.hamzaoui@mines-paristech.fr>
2022-01-28 18:10:23 +0100

    v2

--- projet.py
+++ projet.py
@@ -1,28 +1,24 @@
-from telnetlib import DM
-from this import d
-import pygame as pg
 from random import randint
 from random import choice
+import pygame as pg
 
 # Initialisation
 pg.font.init()
-myfont = pg.font.SysFont('Alerte', 30)
+myfont = pg.font.SysFont('Alerte', 75)
 # pg.init()
 screen = pg.display.set_mode((600, 600))
 clock = pg.time.Clock()
 
 
 # Les variables utiles
-noir = (0,0,0)
-blanc = (255,255,255)
-jaune = (255,255,0)
-gris = (211,211,211)
-bleu = (0,0,255)
+noir = (0, 0, 0)
+blanc = (255, 255, 255)
+jaune = (255, 255, 0)
+gris = (211, 211, 211)
+bleu = (0, 0, 255)
 rouge = (255, 0, 0)
-vert = (0,128,0)
+vert = (0, 128, 0)
 level = 1
-Hits = 12
-Str = 16
 Gold = 0
 Armor = 5
 
@@ -43,20 +39,20 @@ def contour_carre(x1, y1, x2, y2):
     return gauche + droite + haut + bas
 
 
-def trace_rect(x, y, color, width=10, height=10):
-    rect = pg.Rect(x*10, y*10, width, height)
+def trace_rect(a, b, color, width=10, height=10):
+    rect = pg.Rect(a*10, b*10, width, height)
     pg.draw.rect(screen, color, rect)
 
 
 def gen_dj():
-    salles = [(5, 5, 30, 20), (45, 10, 55, 20), (25, 25, 35, 40), (5, 30, 15, 40),
-              (10, 45, 35, 55), (45, 45, 55, 55)]
-    contours = []
+    salles = [(5, 5, 30, 20), (45, 10, 55, 20), (25, 25, 35, 40),
+              (5, 30, 15, 40), (10, 45, 35, 55), (45, 45, 55, 55)]
+    cont = []
     inte = []
     for salle in salles:
-        contours = contours + contour_carre(*salle)
+        cont = cont + contour_carre(*salle)
         inte = inte + interieur_carre(*salle)
-    return contours, inte
+    return cont, inte
 
 
 def couloir():
@@ -66,6 +62,21 @@ def couloir():
     coul = coul + [(i, 30) for i in range(35, 51)]
     coul = coul + [(50, i) for i in range(20, 46)]
     return coul
+
+
+def combat(Arm, lvl, Gld):
+    up = False
+    if (x, y) in mechants:
+        mechants.remove((x, y))
+        damage = randint(0, 3)
+        Arm -= damage
+        if Arm <= 0:
+            return 0, False, Gld
+        elif randint(1, lvl) == 1:
+            up = True
+            Arm = level + 4
+            Gld += randint(0, 3)
+    return Arm, up, Gld
 
 
 # Génère le donjon
@@ -91,6 +102,8 @@ for _ in range(nb):
 
 # Le rendu
 running = True
+time = 30
+lvlup = False
 while running:
     clock.tick(30)
     # 1. lecture des évênements
@@ -109,65 +122,26 @@ while running:
                 if event.key == pg.K_i:
                     if (x, y-1) in interieurs or (x, y-1) in couloirs:
                         y -= 1
-                        if (x, y) in mechants:
-                            mechants.remove((x, y))
-                            damage = randint(0,3)
-                            Armor -= damage
-                            if Armor <= 0:
-                                running = False
-                            elif randint(0, level) == 1:
-                                textsurface = myfont.render('LEVEL UP!', False, blanc)
-                                screen.blit(textsurface, (200,300))
-                                level += 1
-                                Armor = level + 4
-                                Gold += randint(0,3)
+                        Armor, lvlup, Gold = combat(Armor, level, Gold)
+                        level += lvlup
                 elif event.key == pg.K_k:
                     if (x, y+1) in interieurs or (x, y+1) in couloirs:
                         y += 1
                         trace_rect(x, y, jaune)
-                        if (x, y) in mechants:
-                            mechants.remove((x, y))
-                            damage = randint(0,3)
-                            Armor -= damage
-                            if Armor <= 0:
-                                running = False
-                            elif randint(0, level) == 1:
-                                textsurface = myfont.render('LEVEL UP!', False, blanc)
-                                screen.blit(textsurface, (200,300))
-                                level += 1
-                                Armor = level + 4
-                                Gold += randint(0,3)
+                        Armor, lvlup, Gold = combat(Armor, level, Gold)
+                        level += lvlup
                 elif event.key == pg.K_j:
                     if (x-1, y) in interieurs or (x-1, y) in couloirs:
                         x -= 1
-                        if (x, y) in mechants:
-                            mechants.remove((x, y))
-                            damage = randint(0,3)
-                            Armor -= damage
-                            if Armor <= 0:
-                                running = False
-                            elif randint(0, level) == 1:
-                                textsurface = myfont.render('LEVEL UP!', False, blanc)
-                                screen.blit(textsurface, (200,300))
-                                level += 1
-                                Armor = level + 4
-                                Gold += randint(0,3)
+                        Armor, lvlup, Gold = combat(Armor, level, Gold)
+                        if lvlup:
+                            time = 0
+                            level += 1
                 elif event.key == pg.K_l:
                     if (x+1, y) in interieurs or (x+1, y) in couloirs:
                         x += 1
                         trace_rect(x, y, jaune)
-                        if (x, y) in mechants:
-                            mechants.remove((x, y))
-                            damage = randint(0,3)
-                            Armor -= damage
-                            if Armor <= 0:
-                                running = False
-                            elif randint(0, level) == 1:
-                                textsurface = myfont.render('LEVEL UP!', False, blanc)
-                                screen.blit(textsurface, (200,300))
-                                level += 1
-                                Armor = level + 4
-                                Gold += randint(0,3)
+                        Armor, lvlup, Gold = combat(Armor, level, Gold)
 
     # 3. rendu
     trace_rect(0, 0, noir, width=600, height=600)
@@ -176,7 +150,8 @@ while running:
         trace_rect(*contour, blanc)
 
     for interieur in interieurs:
-        trace_rect(interieur[0]+0.5, interieur[1]+0.5, blanc, width=2, height=2)
+        trace_rect(interieur[0]+0.5, interieur[1]+0.5,
+                   blanc, width=2, height=2)
 
     for couloir in couloirs:
         trace_rect(*couloir, gris)
@@ -184,18 +159,37 @@ while running:
     trace_rect(x, y, jaune)
 
     for k, mechant in enumerate(mechants):
-            trace_rect(*mechant, rouge)
+        trace_rect(*mechant, rouge)
 
-    pg.display.set_caption(f"Level: {level}, Hits: {Hits}, Str: {Str}, Gold: {Gold}, Armor: {Armor} ")
+    caracteristiques = f"Level: {level}, Gold: {Gold}, Armor: {Armor}"
+    pg.display.set_caption(caracteristiques)
 
+    if lvlup:
+        time = 0
+        level += 1
+        lvlup = False
+    if time < 30:
+        textsurface = myfont.render('LEVEL UP!', False, vert)
+        screen.blit(textsurface, (175, 275))
+    time += 1
+
+    if Armor == 0 or not mechants:
+        running = False
     pg.display.update()
 
-textsurface = myfont.render('GAME OVER...', False, rouge)
-screen.blit(textsurface, (250,300))
+# Message de fin de jeu
+if mechants:
+    textsurface = myfont.render('GAME OVER...', False, rouge)
+    screen.blit(textsurface, (135, 275))
+else:
+    textsurface = myfont.render('VICTORY', False, vert)
+    screen.blit(textsurface, (200, 275))
 pg.display.update()
 
+# Quitter le jeu
 quitting = True
 while quitting:
+    clock.tick(30)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             quitting = False
